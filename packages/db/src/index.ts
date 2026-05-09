00c8379b248e569427964d1869ee1825b7d94fb5
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-05-09 01:33:16 +0000

    ➕ ADD: Implement closeDb function for database client

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -8,4 +8,8 @@ if (!connectionString) throw new Error('DATABASE_URL is not set');
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
 
+export async function closeDb(): Promise<void> {
+  await client.end();
+}
+
 export * from './schema.js';
