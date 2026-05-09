b0ac95f97359945166ad452d22fbc5bf21a963c6
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-05-09 02:01:11 +0000

    ➕ ADD: Configure Astro with Node adapter
    pnpm astro add node

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   output: 'server',
-  integrations: [react()]
+  integrations: [react()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
