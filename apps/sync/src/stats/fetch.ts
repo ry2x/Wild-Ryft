766e8390314883853133f74bab75979f086bda74
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-05-21 18:04:43 +0000

    ✨ FEAT: Implement stats data synchronization functions
    - Added fetchStatsData and fetchChampionData functions.
    - Integrated data fetching into syncStatsData function.
    - Enhanced error handling for sync operations.

---
+++ apps/sync/src/stats/fetch.ts
@@ -0,0 +1,22 @@
+import { sql } from 'drizzle-orm/sql/sql';
+
+import { championMaster, db } from '@wild-ryft/db';
+import { WRChampionStats } from '../types/wrStatsApi.js';
+import { fetchData } from '../utils.js';
+import { WRConvertChampionIdData } from '../types/stats.js';
+
+export async function fetchStatsData(api: string): Promise<WRChampionStats> {
+  const res = await fetchData<WRChampionStats>(api);
+  return res.data;
+}
+
+export async function fetchChampionData(): Promise<WRConvertChampionIdData[]> {
+  const data = await db
+    .select({
+      heroId: championMaster.heroId,
+      championId: championMaster.champion_id
+    })
+    .from(championMaster)
+    .where(sql`${championMaster.isWr} = true`);
+  return data;
+}
--- apps/sync/src/stats/index.ts
+++ apps/sync/src/stats/index.ts
@@ -1,1 +1,38 @@
-// TODO: implement stats sync
+import { SupportedLocale } from '@wild-ryft/shared';
+import { SyncConfig } from '../types/config.js';
+import { fetchChampionData, fetchStatsData } from './fetch.js';
+import { transformStatsData } from './transform.js';
+import { upsertStatsData } from './upsert.js';
+import { SyncError } from '../error.js';
+
+export interface SyncStatsDataOptions {
+  readonly config: SyncConfig;
+  readonly langs: SupportedLocale[];
+}
+
+export async function syncStatsData(
+  options: SyncStatsDataOptions
+): Promise<void> {
+  try {
+    // Fetch stats data from WR API and champion ID mapping data from DB
+    const wrStatsData = await fetchStatsData(options.config.WR_STATS_URL);
+    const championData = await fetchChampionData();
+
+    // Transform the fetched data into the format required for DB upsert
+    const transformedData = transformStatsData({
+      wrStatsData,
+      convertIdData: championData
+    });
+
+    // Upsert the transformed data into the database
+    await upsertStatsData(transformedData);
+  } catch (err) {
+    if (err instanceof SyncError) throw err;
+    throw new SyncError(
+      'Stats sync failed unexpectedly',
+      'UNKNOWN_ERROR',
+      false,
+      { cause: err }
+    );
+  }
+}
